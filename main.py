a73f0874011a76f44951419cfcbb81088de3f521
gabrielsdev <89107888+gabrielsdev@users.noreply.github.com>
2022-05-23 01:03:57 -0300

    novas funções

--- main.py
+++ main.py
@@ -3,14 +3,30 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+palavras_reservadas = ["if", "int", "else", "return", ]
+simbolos = ["<", "=", ">", "*", "/", "-", "+", "(", ")", ";", "{", "}"]
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def consome_caracter():
+    global count
+    global string
+    i = count
+    count += 1
+    if(count >= len(string)):
+        return -1
+    return string[i]
+
+def transforma_em_string(arq):
+    texto = arq.read()
+    return list(texto)
+
+def identifica_tokens():
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+arq = open("programa.txt", "r")
+count = 0
+string = transforma_em_string(arq)
+x = consome_caracter()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+while(x != -1):
+    if()
+    x = consome_caracter()
